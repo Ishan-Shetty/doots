dca880e35b03b9be9f9bdfacabee67510e1589e5
shetty-ishan-9 <ishanshetty.10@gmail.com>
2024-08-09 16:40:09 +0530

    js file

---
+++ src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <div className="h-screen w-screen absolute bg-gradient-to-b from-black to-sky-950">Hello</div>
+  );
+}
+
+export default App;
--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import './App.css'
-function App() {
-  return (
-   <div className="h-screen w-screen absolute bg-gradient-to-b from-black to-sky-950"></div>
-  );
-}
-
-export default App;
