869ed0a120b65ba02cf7f78639d4d9d18b350304
shetty-ishan-9 <ishanshetty.10@gmail.com>
2024-08-10 14:48:02 +0530

    type animation

--- src/components/Firstpage.tsx
+++ src/components/Firstpage.tsx
@@ -1,7 +1,37 @@
-function Firstpage(){
-    return(
-        <div className="h-screen w-screen absolute bg-gradient-to-b from-black to-sky-950">
+import { Typewriter } from 'react-simple-typewriter';
+
+function Firstpage() {
+  return (
+    <div className="h-screen w-screen bg-gradient-to-b from-black to-sky-950">
+      <div className="flex flex-col justify-center items-center h-full">
+        <h1 className="text-white text-6xl font-bold font-righteous -translate-y-10">
+          WELCOME TO{' '}
+          <span className="text-cyan-600">
+            <Typewriter
+              words={['DOOTS']}
+              loop={1000}
+              cursor
+              cursorStyle="_"
+              typeSpeed={70}
+              deleteSpeed={100}
+              delaySpeed={3000}
+            />
+          </span>
+        </h1>
+        <div className="m-10">
+          <button className="text-white text-2xl font-semibold p-5 hover:bg-cyan-600 rounded-md">
+            Sign In
+          </button>
+          <button className="text-white text-2xl font-semibold p-5 hover:bg-cyan-600 rounded-md">
+            Log In
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
+
 export default Firstpage;
+
+  
+
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,15 @@
 
 
- function Navbar() {
+function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className=" sticky top-0 m-0 p-5 bg-black border-b-2 border-b-cyan-600">
+      <div className="flex flex-row items-center justify-between">
+        <img src="" alt="DOOTS" className="text-cyan-600" />
+        <ul>
+          <li className="text-white font-bold text-xl p-5 hover:bg-cyan-600 rounded-md">ABOUT US</li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 export default Navbar;
